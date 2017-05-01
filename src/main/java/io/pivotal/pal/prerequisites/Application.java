b7873113ed3b36adc0d72e6f5437a803851f8712
Tyson Gern <tgern@pivotal.io>
2017-05-01 15:03:25 -0600

    Initial commit

---
+++ src/main/java/io/pivotal/pal/prerequisites/Application.java
@@ -0,0 +1,19 @@
+package io.pivotal.pal.prerequisites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping
+    public String message() {
+        return "I'm installing dependencies";
+    }
+}
